906fdb6fa144feae97d9abd850d1554b01cc2660
OZLIINEX <elicpx@gmail.com>
2022-08-03 23:56:18 +0800

    first commit

--- frontend/hollow/src/App.vue
+++ frontend/hollow/src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-card>
+    <v-layout>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Hollow
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        bottom
+        temporary
+      >
+        <v-list
+          :items="items"
+        ></v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-card-text>
+          The navigation drawer will appear from the bottom on smaller size screens.
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+  },
+
+  data: () => ({
+    drawer : false,
+    group: null,
+    items: [
+      {
+        title: 'Foo',
+        value: 'Foo',
+        props: {
+          prependIcon: 'mdi-account',
+        }
+      },
+      {
+        "title": 'Bar',
+        "value": 'Bar',
+      },
+      {
+        "title": 'Fizz',
+        "value": 'Fizz',
+      },
+      {
+        "title": 'Buzz',
+        "value": 'Buzz',
+      },
+    ]
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
