e328adb6ce167bad28fe09e35e05ca26396778eb
OZLIINEX <elicpx@gmail.com>
2022-08-16 16:39:11 +0800

    first commit

--- backend/hollow/internal/biz/constant.go
+++ backend/hollow/internal/biz/constant.go
@@ -17,22 +17,22 @@ var (
 
 //TYPES
 type User struct {
-	ID         int64
-	Status     int64
-	Username   string
-	Email      string
-	Phone      int64
-	Created_at int64
-	Deleted_at int64
-	Updated_at int64
-	Password   string
-	Nickname   string
+	ID         int64  //用户ID
+	Status     int64  //用户状态
+	Username   string //用户名
+	Email      string //邮箱
+	Phone      int64  //手机号
+	Created_at int64  //创建时间
+	Deleted_at int64  //删除时间
+	Updated_at int64  //更新时间
+	Password   string //密码
+	Nickname   string //昵称
 }
 
 type Leaf struct {
-	ID        int64
-	Owner     int64
-	Create_at int64
-	Status    int64
-	Message   string
+	ID        int64  //消息ID
+	Owner     int64  //所述对象
+	Create_at int64  //创建时间
+	Status    int64  //消息状态 0=匿名 1=实名 2=封禁
+	Message   string //消息
 }
